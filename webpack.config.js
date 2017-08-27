55891845da41648d7004d4ca5c14da55a9ab940c
amitsrivastava4all <amit.srivastava4all@gmail.com>
2017-08-27 09:52:46 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-    entry:[__dirname+'/app/test.js'],
+    
+    entry:[__dirname+'/app/module1.js',__dirname+'/app/module2.js',],
     output:{
         path:__dirname+'/build',
         filename:'bundle.js'
-    }
+    },
+     module: {
+    loaders: [
+      {
+        test: /\.js$/, // a regular expression that catches .js files
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000, // most common port
+    contentBase: './build',
+    inline: true
+  }
 }
